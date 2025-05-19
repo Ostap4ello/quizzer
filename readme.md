57f69bce3a93fc9df9ef51678e5b657f41e1a0ac
Ostap4ello <okpeleh@gamil.com>
2025-05-19 20:33:24 +0200

    feat: move all frontend-related features into separate class - Display

--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@ Simple question-answer quiz with streamlit frontend.
 
 0. put table with equasions like `<expression> = <result>` in 2nd column into ./tables/ and name it all.xlsx
 1. setup venv, install requirements from ./requirements.txt, enter venv
-2. run ./main.py
+2. run `streamlit run src/main.py`
 3. frontend will be opened in new browser tab, press enter in console to move to each step, <Ctrl-C> to stop
 
 ### Disclaimer: 
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import wrappers.display
 import pandas as pd
 import re
 from random import randint
@@ -8,19 +8,15 @@ def main():
     # Load the data
     df = pd.read_excel("tables/all.xlsx")
 
-    # Display the title
-    questionField = st.empty()
-    answerField = st.empty()
+    disp = wrappers.display.Display()
 
     while True:
         category = 1
+
+        disp.setQuestion("Next Question ?")
+        disp.clearAnswer()
+
         print()
-        questionField.empty()
-        answerField.empty()
-        questionField.markdown(
-            f"<h2 style='text-align: center; color: #eeeeee;'>Next Question?</h2>",
-            unsafe_allow_html=True,
-        )
         input("Press Enter to show next question...")
 
         qIndex = randint(0, len(df))
@@ -33,18 +29,12 @@ def main():
         if match is not None:
             questionText, answerText = match.groups()
 
-        questionField.markdown(
-            f"<h2 style='text-align: center; color: #eeeeee;'>{questionText}</h2>",
-            unsafe_allow_html=True,
-        )
+        disp.setQuestion(questionText)
 
         # Display the answer
         print()
         input("Press Enter to see the answer...")
-        answerField.markdown(
-            f"<h2 style='text-align: center; color: #eeeeee;'>{answerText}</h2>",
-            unsafe_allow_html=True,
-        )
+        disp.setAnswer(answerText)
 
         # Clear the previous output
         print()
