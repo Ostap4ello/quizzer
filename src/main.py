16d57145244d7783079850cae06ebc8709129b4c
Ostap4ello <okpeleh@gmail.com>
2025-11-12 13:32:08 +0100

    fix: rename about me -> about/about the application

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ import streamlit as st
 pg = st.navigation([
     st.Page("pages/quiz.py", title="Quiz", icon=":material/sports_esports:"),
     st.Page("pages/settings.py", title="Settngs", icon=":material/settings:"),
-    st.Page("pages/about_me.py", title="About me", icon=":material/favorite:"),
+    st.Page("pages/about.py", title="About the app", icon=":material/favorite:"),
 ])
 pg.run()
